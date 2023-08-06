3ad4da8f3f65375047b46862786903451c202885
Steve Hudek <sjhudek@gmail.com>
2023-08-06 09:22:57 -0600

    finally!got option 1 to work!

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const { Department, Role, Employee } = require("./database.js");
+const database = require("./database.js");
 const inquirer = require("inquirer");
-const cTable = require('console.table');
+const mysql = require("mysql");
+const cTable = require("console.table");
+  
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "030119Kent**",
+  database: "employee_tracker",
+});
 
 // Display options menu
 function displayOptions() {
@@ -35,10 +46,10 @@ function getUserInput() {
           viewAllDepartments();
           break;
         case "2":
-          viewAllRoles();
+          viewAllRole();
           break;
         case "3":
-          viewAllEmployees();
+          viewAllEmployee();
           break;
         case "4":
           addDepartment();
@@ -66,208 +77,27 @@ function getUserInput() {
 
 // Option 1: View all departments
 function viewAllDepartments() {
-  Department.findAll()
-    .then((departments) => {
-      console.log("\nAll Departments:");
-      if (departments.length === 0) {
-        console.log("No departments found.");
-      } else {
-        console.log("ID\tName");
-        departments.forEach((department) => {
-          console.log(`${department.id}\t${department.name}`);
-        });
-      }
-      displayOptions();
-    })
-    .catch((error) => {
-      console.error("Error retrieving departments:", error);
-      displayOptions();
-    });
-}
-
-
-
-// Option 2: View all roles
-function viewAllRoles() {
-  Role.findAll()
-    .then((roles) => {
-      console.log("\nAll Roles:");
-      roles.forEach((role) => {
-        console.log(
-          `ID: ${role.id}, Title: ${role.title}, Salary: ${role.salary}`
-        );
-      });
-      displayOptions();
-    })
-    .catch((error) => {
-      console.error("Error retrieving roles:", error);
-      displayOptions();
-    });
-}
-
-// Option 3: View all employees
-function viewAllEmployees() {
-  Employee.findAll()
-    .then((employees) => {
-      console.log("\nAll Employees:");
-      employees.forEach((employee) => {
-        console.log(
-          `ID: ${employee.id}, Name: ${employee.firstName} ${employee.lastName}`
-        );
-      });
-      displayOptions();
-    })
-    .catch((error) => {
-      console.error("Error retrieving employees:", error);
-      displayOptions();
-    });
-}
-
-// Option 4: Add a department
-function addDepartment() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "Enter the department name: ",
-      },
-    ])
-    .then((answers) => {
-      const { name } = answers;
-      Department.create({ name })
-        .then(() => {
-          console.log("Department added successfully!");
-          displayOptions();
-        })
-        .catch((error) => {
-          console.error("Error adding department:", error);
-          displayOptions();
-        });
-    });
-}
-
-// Option 5: Add a role
-function addRole() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "Enter the role title: ",
-      },
-      {
-        type: "input",
-        name: "salary",
-        message: "Enter the role salary: ",
-      },
-    ])
-    .then((answers) => {
-      const { title, salary } = answers;
-      Role.create({ title, salary })
-        .then(() => {
-          console.log("Role added successfully!");
-          displayOptions();
-        })
-        .catch((error) => {
-          console.error("Error adding role:", error);
-          displayOptions();
-        });
-    });
-}
-
-// Option 6: Add an employee
-function addEmployee() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "firstName",
-        message: "Enter the employee first name: ",
-      },
-      {
-        type: "input",
-        name: "lastName",
-        message: "Enter the employee last name: ",
-      },
-    ])
-    .then((answers) => {
-      const { firstName, lastName } = answers;
-      Employee.create({ firstName, lastName })
-        .then(() => {
-          console.log("Employee added successfully!");
-          displayOptions();
-        })
-        .catch((error) => {
-          console.error("Error adding employee:", error);
-          displayOptions();
-        });
-    });
-}
-
-// Option 7: Update an employee role
-function updateEmployeeRole() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "employeeId",
-        message: "Enter the employee ID: ",
-      },
-      {
-        type: "input",
-        name: "roleId",
-        message: "Enter the new role ID: ",
-      },
-    ])
-    .then((answers) => {
-      const { employeeId, roleId } = answers;
-      Employee.findByPk(employeeId)
-        .then((employee) => {
-          if (!employee) {
-            console.log("Employee not found!");
-            displayOptions();
-          } else {
-            Role.findByPk(roleId)
-              .then((role) => {
-                if (!role) {
-                  console.log("Role not found!");
-                  displayOptions();
-                } else {
-                  employee
-                    .setRole(role)
-                    .then(() => {
-                      console.log("Employee role updated successfully!");
-                      displayOptions();
-                    })
-                    .catch((error) => {
-                      console.error("Error updating employee role:", error);
-                      displayOptions();
-                    });
-                }
-              })
-              .catch((error) => {
-                console.error("Error retrieving role:", error);
-                displayOptions();
-              });
-          }
-        })
-        .catch((error) => {
-          console.error("Error retrieving employee:", error);
-          displayOptions();
-        });
-    });
+  const query = "SELECT * FROM department";
+  connection.query(query, (err, rows) => {
+    if (err) {
+      console.error("Error retrieving departments:", err);
+    } else {
+      console.table(rows);
+    }
+    displayOptions();
+  });
 }
 
 async function startApp() {
   try {
-    // Sync the defined models with the database
-    await sequelize.sync();
+    // Connect to the database
+    // await connectToDatabase();
+    await connection.connect();
 
     // Display the options menu
     displayOptions();
   } catch (error) {
-    console.error("Error synchronizing database:", error);
+    console.error("Error connecting to database:", error);
   }
 }
 
